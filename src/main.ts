6ad180ca129860548c6e360f942db0e6073e8cc4
natanfds <natanfernandesdossantos@zohomail.com>
2025-09-16 11:42:19 -0300

    feat: carregando env

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = Number(process.env['PORT']) || 3000;
+  console.log(`Running at: http://0.0.0.0:${port}`);
+  await app.listen(port);
 }
-bootstrap();
+
+void bootstrap();
