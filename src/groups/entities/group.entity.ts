c80f8c10084980bd48489eb7595116411c280d87
natanfds <natanfernandesdossantos@zohomail.com>
2025-09-17 21:31:47 -0300

    fix: tirando tabela de group, todas serão tratadas em node

--- src/groups/entities/group.entity.ts
+++
@@ -1,8 +0,0 @@
-import { uuid } from 'drizzle-orm/pg-core';
-import { pgTable, text } from 'drizzle-orm/pg-core';
-
-export const groupEntity = pgTable('groups', {
-  id: uuid('id').defaultRandom().primaryKey(),
-  name: text('name').notNull(),
-  parentId: uuid('parent_id').notNull(),
-});
