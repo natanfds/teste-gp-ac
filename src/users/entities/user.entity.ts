971896bbf48324eb5ed5744454a77d7910e415e2
natanfds <natanfernandesdossantos@zohomail.com>
2025-09-17 21:30:25 -0300

    fix: deixando email unico na tabela de users

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -4,5 +4,5 @@ import { pgTable, text } from 'drizzle-orm/pg-core';
 export const userEntity = pgTable('users', {
   id: uuid('id').defaultRandom().primaryKey(),
   name: text('name').notNull(),
-  email: text('email').notNull(),
+  email: text('email').notNull().unique(),
 });
