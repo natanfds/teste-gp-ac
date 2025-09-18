bb2fde4213e220b205032e9514dff070837d8024
natanfds <natanfernandesdossantos@zohomail.com>
2025-09-18 19:42:28 -0300

    feat: upload de logs para o jaeger ui

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,20 +7,9 @@ import { GroupsModule } from './groups/groups.module';
 import { NodesModule } from './nodes/nodes.module';
 import { DatabaseModule } from './database/database.module';
 import { LoggingMiddleware } from './middleware/logging.middleware';
-import { NodeSDK } from '@opentelemetry/sdk-node';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
 import { APP_FILTER } from '@nestjs/core';
 import { OnExceptionFilter } from './filters/onException.filter';
 
-const sdk = new NodeSDK({
-  instrumentations: [
-    getNodeAutoInstrumentations(),
-    new ExpressInstrumentation(),
-  ],
-});
-
-sdk.start();
 @Module({
   imports: [
     ConfigModule.forRoot({
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NodeSDK } from '@opentelemetry/sdk-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
+import { resourceFromAttributes } from '@opentelemetry/resources';
+import { ATTR_SERVICE_NAME } from '@opentelemetry/semantic-conventions';
+import { BatchSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+
+  // Configuração do OpenTelemetry
+  const traceExporter = new OTLPTraceExporter({
+    url: String(configService.get('TRACE_EXPORT_URL')),
+  });
+
+  const sdk = new NodeSDK({
+    instrumentations: [
+      getNodeAutoInstrumentations(),
+      new ExpressInstrumentation(),
+    ],
+    resource: resourceFromAttributes({
+      [ATTR_SERVICE_NAME]: 'api-service',
+    }),
+    spanProcessor: new BatchSpanProcessor(traceExporter),
+  });
+
+  sdk.start();
+
   const port = Number(process.env['PORT']) || 3000;
   app.useGlobalPipes(
     new ValidationPipe({
@@ -12,7 +40,17 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
   await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
+
+  process.on('SIGTERM', () => {
+    sdk
+      .shutdown()
+      .then(() => console.log('Tracing terminated'))
+      .catch((error) => console.error('Error terminating tracing', error))
+      .finally(() => process.exit(0));
+  });
 }
 
 void bootstrap();
