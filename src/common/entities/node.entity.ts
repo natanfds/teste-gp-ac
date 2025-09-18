485165b221c6230f3e1de966e13cb4f176185483
natanfds <natanfernandesdossantos@zohomail.com>
2025-09-17 21:29:33 -0300

    fix: parent id na tabela de nodeEntity ao invés de depth

--- src/common/entities/node.entity.ts
+++ src/common/entities/node.entity.ts
@@ -1,7 +1,7 @@
-import { integer, uuid, pgTable, text } from 'drizzle-orm/pg-core';
+import { uuid, pgTable, text } from 'drizzle-orm/pg-core';
 
 export const nodeEntity = pgTable('nodes', {
   id: uuid('id').defaultRandom().primaryKey(),
   name: text('name').notNull(),
-  depth: integer('depth').notNull(),
+  parentId: uuid('parent_id'),
 });
